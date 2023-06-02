30c78b87ce03fe27c57227c70f028c6c634aa72b
Michal Ilski <michal_ilski@icloud.com>
2023-06-02 19:44:59 +0200

    docker compose

--- app/core/db/models.py
+++ app/core/db/models.py
@@ -1,16 +1,14 @@
 from datetime import datetime
 from typing import List
 
-from pydantic import BaseModel
 
-
-class Movie(BaseModel):
+class Movie:
     # TODO fill based on imdb
     imdb_id: str
     title: str
 
 
-class MoviePlaytime(BaseModel):
+class MoviePlaytime:
     # TODO fill with necessary fields
     movie: Movie
     dates: List[datetime]
