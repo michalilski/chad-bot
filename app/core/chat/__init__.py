310587abbdc941cae4f92baa88ebd3e77fd4ee4b
Michal Ilski <michal_ilski@icloud.com>
2023-06-02 16:54:21 +0200

    dm poc

--- app/core/chat/__init__.py
+++ app/core/chat/__init__.py
@@ -1,8 +1,8 @@
 from app.core.chat.chatgpt_handler import ChatGPTHandler
 from app.core.chat.dialogue_handler import DialogueHandler
-from app.core.chat.dst_module import ChatGPTBasedDSTModule
-from app.core.chat.intent_detection import ChatGPTBasedIntentDetectionModule
-from app.core.chat.response_generation import ChatGPTBasedResponseGenerationModule
+from app.core.chat.dst.dst_module import ChatGPTBasedDSTModule
+from app.core.chat.dst.intent_detection import ChatGPTBasedIntentDetectionModule
+from app.core.chat.nlg.response_generation import ChatGPTBasedResponseGenerationModule
 
 chatgpt_handler: ChatGPTHandler = ChatGPTHandler()
 chatgpt_based_dialogue_handler: DialogueHandler = DialogueHandler(
